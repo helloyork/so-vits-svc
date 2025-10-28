4901cbf26f0c38faf2c0c1555ce2e9c6a23bfb7e
Nomen <111544765+helloyork@users.noreply.github.com>
2025-10-27 21:38:43 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -60,8 +60,9 @@ def plot_data_to_numpy(x, y):
     plt.tight_layout()
 
     fig.canvas.draw()
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    data = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (4,))
+
     plt.close()
     return data
 
@@ -263,8 +264,8 @@ def plot_spectrogram_to_numpy(spectrogram):
   plt.tight_layout()
 
   fig.canvas.draw()
-  data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-  data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+  data = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
+  data = data.reshape(fig.canvas.get_width_height()[::-1] + (4,))
   plt.close()
   return data
 
@@ -292,8 +293,8 @@ def plot_alignment_to_numpy(alignment, info=None):
   plt.tight_layout()
 
   fig.canvas.draw()
-  data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-  data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+  data = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
+  data = data.reshape(fig.canvas.get_width_height()[::-1] + (4,))
   plt.close()
   return data
 
