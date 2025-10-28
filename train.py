a96e231a582e6b3027d5c61b6be6c2f09e4fed04
Nomen <111544765+helloyork@users.noreply.github.com>
2025-10-27 18:31:17 -0700

    Replace TensorBoard import with tensorboardX

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from torch.cuda.amp import GradScaler, autocast
 from torch.nn import functional as F
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 
 import modules.commons as commons
 import utils
